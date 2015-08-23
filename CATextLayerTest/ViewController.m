f46c4ce6ff7b2cc45b67946f1c2d046a4a226918
Joe Shang <shangchuanren@gmail.com>
2015-08-23 16:28:43 +0800

    [Refactor]将 fontSize 作为动画的属性，并修正一些代码规范问题

--- CATextLayerTest/ViewController.m
+++ CATextLayerTest/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 static CGFloat const kTextLayerFontSize = 29.0f;
+static CGFloat const kTextLayerSelectedFontSize = 35.0f;
 static CGFloat const kLayerWidth = 300.0f;
 static CGFloat const kLayerHeight = 40.0f;
 static CGFloat const kAnimationDuration = 5.0f;
@@ -24,79 +25,13 @@ static CGFloat const kAnimationDuration = 5.0f;
 
 @implementation ViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.colorTextLayer = [CATextLayer layer];
-    self.colorTextLayer.string = @"我会整体变色哦";
-    [self p_commonInitTextLayer:self.colorTextLayer];
-    [self.view.layer addSublayer:self.colorTextLayer];
-    
-    self.topLayer = [CALayer layer];
-    self.topLayer.backgroundColor = [UIColor blueColor].CGColor;
-    self.topLayer.frame = CGRectMake(0, 0, 0, kLayerHeight);
-    
-    self.bottomLayer = [CALayer layer];
-    self.bottomLayer.backgroundColor = [UIColor blackColor].CGColor;
-    self.bottomLayer.bounds = CGRectMake(0, 0, kLayerWidth, kLayerHeight);
-    self.bottomLayer.speed = 0;
-    [self.bottomLayer addSublayer:self.topLayer];
-    [self.view.layer addSublayer:self.bottomLayer];
-    
-    self.maskTextLayer = [CATextLayer layer];
-    self.maskTextLayer.string = @"我会区域变色哦";
-    self.maskTextLayer.foregroundColor = [UIColor whiteColor].CGColor;
-    [self p_commonInitTextLayer:self.maskTextLayer];
-    self.maskTextLayer.frame = self.bottomLayer.bounds;
-    self.bottomLayer.mask = self.maskTextLayer;
-    
-    CABasicAnimation *colorAnimation = [CABasicAnimation
-                                        animationWithKeyPath:@"foregroundColor"];
-    colorAnimation.duration = kAnimationDuration;
-    colorAnimation.fillMode = kCAFillModeForwards;
-    colorAnimation.removedOnCompletion = NO;
-    colorAnimation.fromValue = (id)[UIColor blackColor].CGColor;
-    colorAnimation.toValue = (id)[UIColor redColor].CGColor;
-    colorAnimation.timingFunction = [CAMediaTimingFunction
-                                     functionWithName:kCAMediaTimingFunctionLinear];
-    
-    CABasicAnimation *scaleAnimation = [CABasicAnimation
-                                        animationWithKeyPath:@"transform"];
-    scaleAnimation.duration = kAnimationDuration;
-    scaleAnimation.fillMode = kCAFillModeForwards;
-    scaleAnimation.removedOnCompletion = NO;
-    scaleAnimation.fromValue = (id)[NSValue valueWithCATransform3D:CATransform3DScale(self.colorTextLayer.transform, 1, 1, 1)];
-    scaleAnimation.toValue = (id)[NSValue valueWithCATransform3D:CATransform3DScale(self.colorTextLayer.transform, 1.5, 1.5, 1)];
-    scaleAnimation.timingFunction = [CAMediaTimingFunction
-                                     functionWithName:kCAMediaTimingFunctionLinear];
-    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
-    animationGroup.duration = kAnimationDuration;
-    animationGroup.timingFunction = [CAMediaTimingFunction
-                                     functionWithName:kCAMediaTimingFunctionLinear];
-    animationGroup.fillMode = kCAFillModeForwards;
-    animationGroup.removedOnCompletion = NO;
-    animationGroup.animations =
-    [NSArray arrayWithObjects:colorAnimation, scaleAnimation, nil];
-    
-    [self.colorTextLayer addAnimation:animationGroup forKey:@"animateColorWithScale"];
-    
-    scaleAnimation.fromValue = (id)[NSValue valueWithCATransform3D:CATransform3DScale(self.bottomLayer.transform, 1, 1, 1)];
-    scaleAnimation.toValue = (id)[NSValue valueWithCATransform3D:CATransform3DScale(self.bottomLayer.transform, 1.5, 1.5, 1)];
-    [self.bottomLayer addAnimation:scaleAnimation forKey:@"animateScale"];
-}
-
-- (IBAction)onSliderValueChanged:(id)sender {
-    UISlider *slider = (UISlider *)sender;
-    
-    self.colorTextLayer.timeOffset = slider.value;
-    self.bottomLayer.timeOffset = slider.value;
-    
-    [CATransaction begin];
-    [CATransaction setDisableActions:YES];
-    CGRect rect = self.topLayer.frame;
-    rect.size.width = roundf(self.bottomLayer.frame.size.width * slider.value / kAnimationDuration);
-    self.topLayer.frame = rect;
-    [CATransaction commit];
+    [self p_initLayers];
+    [self p_addAnimationForLayers];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -109,17 +44,95 @@ static CGFloat const kAnimationDuration = 5.0f;
     self.bottomLayer.position = center;
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Target-Action
+
+- (IBAction)onSliderValueChanged:(id)sender {
+    UISlider *slider = (UISlider *)sender;
+    
+    self.colorTextLayer.timeOffset = slider.value;
+    self.maskTextLayer.timeOffset = slider.value;
+    
+    [CATransaction begin];
+    [CATransaction setDisableActions:YES];
+    CGRect rect = self.topLayer.frame;
+    rect.size.width = roundf(self.bottomLayer.frame.size.width * slider.value / kAnimationDuration);
+    self.topLayer.frame = rect;
+    [CATransaction commit];
+}
+
+#pragma mark - Private
+
+- (void)p_initLayers {
+    self.colorTextLayer = [CATextLayer layer];
+    self.colorTextLayer.string = @"我会整体变色哦";
+    self.colorTextLayer.foregroundColor = [UIColor blackColor].CGColor;
+    [self p_commonInitTextLayer:self.colorTextLayer];
+    [self.view.layer addSublayer:self.colorTextLayer];
+    
+    self.topLayer = [CALayer layer];
+    self.topLayer.backgroundColor = [UIColor blueColor].CGColor;
+    self.topLayer.frame = CGRectMake(0, 0, 0, kLayerHeight);
+    
+    self.bottomLayer = [CALayer layer];
+    self.bottomLayer.backgroundColor = [UIColor blackColor].CGColor;
+    self.bottomLayer.bounds = CGRectMake(0, 0, kLayerWidth, kLayerHeight);
+    [self.bottomLayer addSublayer:self.topLayer];
+    [self.view.layer addSublayer:self.bottomLayer];
+    
+    self.maskTextLayer = [CATextLayer layer];
+    self.maskTextLayer.string = @"我会区域变色哦";
+    self.maskTextLayer.foregroundColor = [UIColor whiteColor].CGColor;
+    [self p_commonInitTextLayer:self.maskTextLayer];
+    self.maskTextLayer.frame = self.bottomLayer.bounds;
+    self.bottomLayer.mask = self.maskTextLayer;
+}
+
+- (void)p_addAnimationForLayers {
+    
+    CABasicAnimation *colorAnimation = [CABasicAnimation
+                                        animationWithKeyPath:@"foregroundColor"];
+    colorAnimation.duration = kAnimationDuration;
+    colorAnimation.fillMode = kCAFillModeForwards;
+    colorAnimation.removedOnCompletion = NO;
+    colorAnimation.fromValue = (id)[UIColor blackColor].CGColor;
+    colorAnimation.toValue = (id)[UIColor redColor].CGColor;
+    colorAnimation.timingFunction = [CAMediaTimingFunction
+                                     functionWithName:kCAMediaTimingFunctionLinear];
+    
+    CABasicAnimation *scaleAnimation = [CABasicAnimation
+                                        animationWithKeyPath:@"fontSize"];
+    scaleAnimation.duration = kAnimationDuration;
+    scaleAnimation.fillMode = kCAFillModeForwards;
+    scaleAnimation.removedOnCompletion = NO;
+    scaleAnimation.fromValue = @(kTextLayerFontSize);
+    scaleAnimation.toValue = @(kTextLayerSelectedFontSize);
+    scaleAnimation.timingFunction = [CAMediaTimingFunction
+                                     functionWithName:kCAMediaTimingFunctionLinear];
+    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
+    animationGroup.duration = kAnimationDuration;
+    animationGroup.timingFunction = [CAMediaTimingFunction
+                                     functionWithName:kCAMediaTimingFunctionLinear];
+    animationGroup.fillMode = kCAFillModeForwards;
+    animationGroup.removedOnCompletion = NO;
+    animationGroup.animations = @[colorAnimation, scaleAnimation];
+    
+    self.colorTextLayer.speed = 0.0f;
+    [self.colorTextLayer addAnimation:animationGroup forKey:@"animateColorAndFontSize"];
+    
+    self.maskTextLayer.speed = 0.0f;
+    [self.maskTextLayer addAnimation:scaleAnimation forKey:@"animateFontSize"];
+}
+
 - (void)p_commonInitTextLayer:(CATextLayer *)textLayer {
     textLayer.fontSize = kTextLayerFontSize;
     textLayer.contentsScale = [[UIScreen mainScreen] scale];
     textLayer.alignmentMode = kCAAlignmentCenter;
-    textLayer.speed = 0.0f;
     textLayer.frame = CGRectMake(0, 0, kLayerWidth, kLayerHeight);
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
